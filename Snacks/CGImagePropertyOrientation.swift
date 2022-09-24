a6cee9aff42b586ec30861433caaff54af333b37
4zyh <106859204+4zyh@users.noreply.github.com>
2022-09-24 16:16:10 +0000

    Version Inicial

--- Snacks/CGImagePropertyOrientation.swift
+++ Snacks/CGImagePropertyOrientation.swift
@@ -6,4 +6,4 @@ extension CGImagePropertyOrientation {
     case .up: self = .up
     case .upMirrored: self = .upMirrored
     case .down: self = .down
-    case .downM
+    case .downMirrored: self =
